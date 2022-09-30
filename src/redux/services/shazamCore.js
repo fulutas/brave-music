a8363a487faaba6ea033252e25a41df28d3b5045
Furkan Ulutaş <ulutasfurkan@gmail.com>
2022-09-30 18:51:56 +0300

    Updated component AroundYou

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,8 +17,15 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
     getSongDetails : builder.query({ query : ({ songid }) => `/tracks/details?track_id=${songid}`}),
     getSongRelated : builder.query({ query : ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    getArtistDetails : builder.query({ query : ({ artistId }) => `/artists/details?artist_id=${artistId}`})
+    getArtistDetails : builder.query({ query : ({ artistId }) => `/artists/details?artist_id=${artistId}`}),
+    getSongsByCountry : builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
   }),
 });
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery
+} = shazamCoreApi;
